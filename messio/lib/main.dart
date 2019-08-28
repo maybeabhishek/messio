93093759850e17833a33cd6032593f8de88b5c2b
abhishek <abhishek.satapathy01@gmail.com>
2019-08-28 10:21:05 +0530

    updated travis yml file

--- messio/lib/main.dart
+++ messio/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Messio',
       theme: ThemeData(
-        primarySwatch: Colors.yellow,
+        primarySwatch: Colors.cyan,
       ),
       home: MyHomePage(title: 'Messio'),
     );
